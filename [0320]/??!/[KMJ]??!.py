297217dc2971f08bac2512f3a26e6ada4b94046d
MinJi Kim (Mindal) <108729047+mjkim0819@users.noreply.github.com>
2023-03-15 22:40:52 +0900

    Create [KMJ]??!.py

---
+++ [0320]/??!/[KMJ]??!.py
@@ -0,0 +1,15 @@
+name = input()
+print(name+'??!')
+
+
+
+# print의 괄호 안을 ,로 구분하면 줄 넘김 후 출력하고 +로 구분하면 함께 출력된다.
+
+name = MINJI
+print(name,'??!')
+=> MINJI
+=> ??!
+
+name = MINJI
+print(name+'??!')
+=> MINJI??!
