05d5ea2626ca473fe3fbf4c059b36f4e4124e732
ParkJooHyeung <59814944+ParkJooHyeung@users.noreply.github.com>
2023-03-17 17:18:20 +0900

    Create [PJH]사칙연산.py

---
+++ [0320]/사칙연산/[PJH]사칙연산.py
@@ -0,0 +1,10 @@
+# 수를 입력 받고 쓰기 위해 readline함수를 중심으로 코딩 작성
+from sys import stdin # readline함수를 사용하기 위해 stdin을 import
+
+num1, num2 = map(int, stdin.readline().split()) # 두 정수를 구분하기 위해 공백을 넣어 준 후 map을 이용하여 정수형태로 전환
+
+print(num1 + num2) # 입력 받은 두 정수를 더한 후 몫을 출력
+print(num1 - num2) # 입력 받은 두 정수를 뺀 후 몫을 출력
+print(num1 * num2) # 입력 받은 두 정수를 곱한 후 몫을 출력
+print(int(num1 / num2)) # 입력 받은 두 정수를 나눈 후 int형태로 전환한 몫을 출력
+print(num1 % num2) # 입력 받은 두 정수를 나눈 후 나머지를 출력
