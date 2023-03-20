0a5a4289a5379e58a2344ec995fcefef7490a4a7
kimdw6920 <38483620+kimdw6920@users.noreply.github.com>
2023-03-20 15:52:09 +0900

    Create [KDW]사칙연산.py

---
+++ [0320]/사칙연산/[KDW]사칙연산.py
@@ -0,0 +1,16 @@
+a,b = map(int,input().split())
+# 덧셈 연산
+c = a + b
+print(c)
+# 뺄셈 연산
+c = a - b
+print(c)
+# 곱 연산
+c = a * b
+print(c)
+# 나누기 연산( //연산자를 사용하여 정수형으로 몫을 출력)
+c = a // b
+print(c)
+# 나머지 구하는 연산
+c = a % b
+print(c)
