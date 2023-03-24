44998af43e755f806ad5acd7068223f04f86735c
sj990710 <127752372+sj990710@users.noreply.github.com>
2023-03-24 21:28:50 +0900

    Delete [0327]/[코딩]/두_수_비교하기 directory

--- [0327]/[코딩]/두_수_비교하기/[PJH]두 수 비교하기.py
+++
@@ -1,7 +0,0 @@
-a, b = map(int, input().split())
-if a > b: # 정수로 입력받은 a와 b를 비교하여 a가 b를 초과할 경우를 조건으로 사용
-    print(">")
-elif a < b: # 정수로 입력받은 a와 b를 비교하여 b가 a를 초과할 경우를 조건을 사용
-    print("<")
-else: # 위의 두 경우를 제외 나머지 a와 b가 같을경우를 else로 취급하여 조건으로 사용
-    print("==")
