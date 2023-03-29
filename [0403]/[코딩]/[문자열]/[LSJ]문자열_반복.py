f2c19b323cd51c2ecac38e28faffbbed05fe87e9
sj990710 <127752372+sj990710@users.noreply.github.com>
2023-03-29 15:22:56 +0900

    Delete [LSJ]문자열_반복.py

--- [0403]/[코딩]/[문자열]/[LSJ]문자열_반복.py
+++
@@ -1,8 +0,0 @@
-T = int(input())
-for i in range(T):
-    R, S=list(map(str,input().split()))
-    for j in range(len(S)):
-        print(int(R)*S[j], end="")
-    if i==(T-1):
-        break
-    print("")
