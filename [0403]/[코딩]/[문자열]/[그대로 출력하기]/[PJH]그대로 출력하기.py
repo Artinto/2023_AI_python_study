ea6e9bbbd015cfaeab093cd3ee4adc24497a53c2
ParkJooHyeung <59814944+ParkJooHyeung@users.noreply.github.com>
2023-04-03 21:16:46 +0900

    Update [PJH]그대로 출력하기.py

--- [0403]/[코딩]/[문자열]/[그대로 출력하기]/[PJH]그대로 출력하기.py
+++ [0403]/[코딩]/[문자열]/[그대로 출력하기]/[PJH]그대로 출력하기.py
@@ -4,4 +4,4 @@ while True :
     except EOFError :
         break
 # 입력이 끝날때 종료를 위해 EOF Error를
-#EOEF란 입력값이 없어지는 상황을 받아주는 것이다. error가 났을 때 break를 걸어준다
+#EOFE란 입력값이 없어지는 상황을 받아주는 것이다. error가 났을 때 break를 걸어준다
