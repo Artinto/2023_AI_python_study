9a6e44058445b811b26377104f48f8c36daf0768
ParkJooHyeung <59814944+ParkJooHyeung@users.noreply.github.com>
2023-04-09 20:03:57 +0900

    Create [PJH]너의 평점.py

---
+++ [0410]/[심화1]/[너의 평점]/[PJH]너의 평점.py
@@ -0,0 +1,15 @@
+rating = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+grade = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+
+total = 0
+result = 0
+
+for _ in range(20):
+    s, p, g = input().split()
+    p = float(p)
+    if g != 'P':
+        total += p
+        result += p * grade[rating.index(g)] # g의 인덱스를 rating과 비교하여 같은 인덱스를 가진 grade의 값을 찾아냄
+
+print('%.6f' % (result/total))
+
