dd6ee2008d41c3d6f782913f3b93e638623874ca
eunseooooo <127365253+eunseooooo@users.noreply.github.com>
2023-04-17 20:33:53 +0900

    Create [CES]10_1_cnn_mnist.py

---
+++ [0417]/[AI]/[10_1_cnn_mnist]/[CES]10_1_cnn_mnist.py
@@ -0,0 +1,94 @@
+# https://github.com/pytorch/examples/blob/master/mnist/main.py
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+# 라이브러리 불러오기
+# Training settings
+batch_size = 64
+#하이퍼파라미터 설정
+
+train_dataset = datasets.MNIST(root='./data/',
+                               train=True,
+                               transform=transforms.ToTensor(),
+                               download=True)
+
+test_dataset = datasets.MNIST(root='./data/',
+                              train=False,
+                              transform=transforms.ToTensor())
+# MNIST 데이터셋을 불러옴
+# transforms.ToTensor()를 이용하여 데이터를 텐서 형태로 변환하고, DataLoader를 이용하여 데이터를 불옴
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+
+class Net(nn.Module):
+# nn.Module을 상속받아 모델을 구성
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.mp = nn.MaxPool2d(2)
+        self.fc = nn.Linear(320, 10)
+
+    def forward(self, x):
+        in_size = x.size(0)
+        x = F.relu(self.mp(self.conv1(x)))
+        x = F.relu(self.mp(self.conv2(x)))
+        x = x.view(in_size, -1)  # flatten the tensor
+        x = self.fc(x)
+        return F.log_softmax(x)
+
+# 두 개의 합성곱(Convolution) 레이어와 두 개의 최대 풀링(Max Pooling) 레이어 그리고 
+# 하나의 선형(Linear) 레이어로 이루어진 모델을 구성
+model = Net()
+
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+# 옵티마이저 설정
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+# 학습 함수 정의
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        # sum up batch loss
+        test_loss += F.nll_loss(output, target, size_average=False).data
+        # get the index of the max log-probability
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+# FOR 반복문을 돌려 10번 
+for epoch in range(1, 10):
+    train(epoch)
+    test()
