850be38d58a4c3d48563810cae9da745ac7d6e65
Seungbeom oh <114134484+Sbeom12@users.noreply.github.com>
2023-05-01 20:37:46 +0900

    Delete [CES]나이순_정렬.py

--- [0501]/[코딩]/[정렬]/[나이순_정렬]/[CES]나이순_정렬.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-li = []
-for i in range(N):
-    age, name = map(str, input().split())
-    age = int(age)
-    li.append((age, name))
-li.sort(key = lambda x : x[0])
-for i in li:
-    print(i[0],i[1])
