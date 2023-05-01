e69186b2f843abed80a6a3222de1c4737562682a
Seungbeom oh <114134484+Sbeom12@users.noreply.github.com>
2023-05-01 20:36:02 +0900

    Delete [LSJ]블랙잭.py

--- [0501]/[코딩]/[브루트 포스]/[블랙잭]/[LSJ]블랙잭.py
+++
@@ -1,9 +0,0 @@
-from itertools import combinations 
-N, M = map(int, input().split())
-cards=list(map(int,input().split()))
-ans=0
-for cards in combinations(cards, 3):
-    S = sum(cards)
-    if ans<S<=M:
-        ans=S
-print(ans)
