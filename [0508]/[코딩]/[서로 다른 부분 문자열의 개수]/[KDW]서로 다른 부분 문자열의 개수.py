eb467b71eb62f1e6e6c4f8d401d7e3a060652303
kimdw6920 <38483620+kimdw6920@users.noreply.github.com>
2023-05-08 02:17:51 +0900

    Create [KDW]서로 다른 부분 문자열의 개수.py

---
+++ [0508]/[코딩]/[서로 다른 부분 문자열의 개수]/[KDW]서로 다른 부분 문자열의 개수.py
@@ -0,0 +1,8 @@
+S=input()
+total=set()
+for i in range(len(S)):
+    for j in range(i,len(S)):
+        total.add(S[i:j+1])#i번째 문자부터 j번째 문자까지 부분 문자열 J+1을 통해 J까지
+        # S="abc" 일 때,  i=0, j=2일 때는 S [0:2] = "ab"가 추가 이 연산이 반복되면서 a부터 ababc까지 모두 반복
+
+print(len(total))
