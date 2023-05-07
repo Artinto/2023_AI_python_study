39f654d8f7e2f6282295c8db9c35cde84e0ab68f
Seungbeom oh <114134484+Sbeom12@users.noreply.github.com>
2023-05-07 22:25:02 +0900

    Delete [PJH]대칭 차집합.py

--- [0508]/[코딩]/[대칭 차집합]/[PJH]대칭 차집합.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-count_A, count_B = map(int, input().split())
-
-A = set(map(int,sys.stdin.readline().split()))
-B = set(map(int,sys.stdin.readline().split()))
-
-print(len(A-B)+len(B-A))
