273629aa35a27012c08fa0d0666d8473d7e106f8
sj990710 <127752372+sj990710@users.noreply.github.com>
2023-05-13 21:39:24 +0900

    Create [LSJ]균형잡힌_세상.py

---
+++ [0515]/[코딩]/[스택]/[균형잡힌_세상]/[LSJ]균형잡힌_세상.py
@@ -0,0 +1,24 @@
+while 1:
+    stack = []
+    target = input()
+    if target == '.':
+        break
+    for i in target:
+        if i == '(' or i == '[':
+            stack.append(i)
+        elif i == ')':
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(')')
+                break
+        elif i == ']':
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(']')
+                break
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
