89a31c392e4a70864b2e934e315fa711106f8ddb
eunseooooo <127365253+eunseooooo@users.noreply.github.com>
2023-05-22 20:10:42 +0900

    Update [CES]카드2.py

--- [0515]/[코딩]/[큐, 덱 단계]/[카드2]/[CES]카드2.py
+++ [0515]/[코딩]/[큐, 덱 단계]/[카드2]/[CES]카드2.py
@@ -6,11 +6,15 @@ arr = [i+1 for i in range(N)]
 
 while len(arr) > 1:
     if len(arr) % 2:
+        # arr의 길이가 홀수인지 확인
         t = [arr[-1]]
+        # 변수 t에 arr의 마지막 요소를 담은 리스트를 할당
         t.extend(arr[1::2])
+        #  arr의 두 번째 요소부터 인덱스가 홀수인 요소들을 t에 추가
         arr = t
+        # arr을 t로 갱신
     else:
         arr = arr[1::2]
-        
+        # arr을 두 번째 요소부터 인덱스가 홀수인 요소들로 갱신
 
 print(arr[0])
