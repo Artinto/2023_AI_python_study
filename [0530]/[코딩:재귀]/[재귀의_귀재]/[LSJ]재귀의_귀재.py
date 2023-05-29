1242173e1adb6af22bd420b436e5600f39485dd1
sj990710 <127752372+sj990710@users.noreply.github.com>
2023-05-30 02:23:37 +0900

    Update [LSJ]재귀의_귀재.py

--- [0530]/[코딩:재귀]/[재귀의_귀재]/[LSJ]재귀의_귀재.py
+++ [0530]/[코딩:재귀]/[재귀의_귀재]/[LSJ]재귀의_귀재.py
@@ -1,3 +1,18 @@
+def recursion(S, i, j, cnt):
+    cnt += 1
+    if i >= j: # 1 1 0 1 1
+        return 1, cnt
+    elif S[i] != S[j]: # i != -i
+        return 0, cnt
+    else:
+        return recursion(S, i + 1, j - 1, cnt) # 0 vs -1 -> 1 vs -2
+def isPalindrome(S):
+    return recursion(S, 0, len(S) - 1, 0) # 문자열 S. 0, -1까지
+T=int(input())
+for _ in range(T):
+    S = input()
+    print(*isPalindrome(S))
+'''
 def recursion(S, i, j):
     global cnt  # 함수 내에서 전역 변수로 cnt를 활용하기 위해 global로 명시해준다.
     cnt += 1
@@ -14,3 +29,4 @@ for _ in range(T):
     S = input()
     cnt =0
     print(isPalindrome(S), cnt)
+'''
