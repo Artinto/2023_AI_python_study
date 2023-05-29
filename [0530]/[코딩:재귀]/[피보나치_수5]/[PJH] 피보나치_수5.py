a48ce0ee0c0c2a32a8752212a95108b3b7623862
ParkJooHyeung <59814944+ParkJooHyeung@users.noreply.github.com>
2023-05-30 04:16:27 +0900

    Update [PJH] 피보나치_수5.py

--- [0530]/[코딩:재귀]/[피보나치_수5]/[PJH] 피보나치_수5.py
+++ [0530]/[코딩:재귀]/[피보나치_수5]/[PJH] 피보나치_수5.py
@@ -1,7 +1,8 @@
 def fibonacci(n):
-    if n <= 1:
-        return n
-    return fibonacci(n-1) + fibonacci(n-2) # fibonacci를 계속 불러와 해당 타겟 목표까지 반복
+    if n <= 1:  # n이 1 이하인 경우
+        return n  # n을 그대로 반환
+    
+    return fibonacci(n-1) + fibonacci(n-2)  # n번째 피보나치 수를 계산하기 위해 n-1번째와 n-2번째 피보나치 수를 더함
 
-n = int(input())
-print(fibonacci(n))
+n = int(input())  # n 입력
+print(fibonacci(n))  # n번째 피보나치 수 출력
