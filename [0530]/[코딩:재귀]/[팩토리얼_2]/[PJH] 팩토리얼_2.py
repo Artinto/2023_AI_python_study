eb8d4643a938488b8426400cb1297ab76b287685
ParkJooHyeung <59814944+ParkJooHyeung@users.noreply.github.com>
2023-05-30 04:17:33 +0900

    Update [PJH] 팩토리얼_2.py

--- [0530]/[코딩:재귀]/[팩토리얼_2]/[PJH] 팩토리얼_2.py
+++ [0530]/[코딩:재귀]/[팩토리얼_2]/[PJH] 팩토리얼_2.py
@@ -1,11 +1,11 @@
-N = int(input())
-num = 1
-pec = 1
+N = int(input())  # 정수 N 입력
+num = 1  # 변수 초기화: 곱셈 결과를 저장할 변수
+pec = 1  # 변수 초기화: 팩토리얼을 계산할 변수
 
-if N >= 0:
-  if N > 0:
-    for i in range(1, N):
-      pec = pec * (N+1-i) # N, N-1, N-2 ... 이러한 순서로 곱할 것이기 때문에 N+1-i로 해준다.
-  elif N == 0:
-    pec = 1
-print(pec)
+if N >= 0:  # N이 0 이상인 경우
+    if N > 0:  # N이 0보다 큰 경우
+        for i in range(1, N):  # 1부터 N-1까지 반복
+            pec = pec * (N+1-i)  # pec에 N, N-1, N-2, ... 순서로 곱셈을 수행합니다.
+    elif N == 0:  # N이 0인 경우
+        pec = 1  # pec는 1이 됩니다.
+print(pec)  # pec 출력
