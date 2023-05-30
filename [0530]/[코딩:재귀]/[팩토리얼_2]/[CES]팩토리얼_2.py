f6f3a5c00467e736de8f50d4fb28635f00065a65
eunseooooo <127365253+eunseooooo@users.noreply.github.com>
2023-05-30 21:23:16 +0900

    Update [CES]팩토리얼_2.py

--- [0530]/[코딩:재귀]/[팩토리얼_2]/[CES]팩토리얼_2.py
+++ [0530]/[코딩:재귀]/[팩토리얼_2]/[CES]팩토리얼_2.py
@@ -3,3 +3,14 @@ P = 1
 for i in range(1, N+1):
     P*=i
 print(P)
+
+import math 
+
+
+def factorial_2(N):
+    return math.factorial(N)
+    
+
+if __name__ == "__main__":
+    N = int(input())
+    print(factorial_2(N=N))
