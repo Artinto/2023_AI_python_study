9e7115334c5ff2e9109b270aec096d1575a4f580
eunseooooo <127365253+eunseooooo@users.noreply.github.com>
2023-06-26 13:26:50 +0900

    Create [CES]연산자 끼워넣기.py

---
+++ [0619]/[백트래킹]/[연산자 끼워넣기]/[CES]연산자 끼워넣기.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+num = list(map(int, input().split()))
+op = list(map(int, input().split()))  # +, -, *, //
+
+maximum = -1e9
+minimum = 1e9
+
+
+def dfs(depth, total, plus, minus, multiply, divide):
+    global maximum, minimum
+    if depth == N: # 모든 숫자의 계산이 완료된 경우 동작
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
+
+    if plus:
+        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
+      # 깊이를 1씩 증가시키고 현재까지의 계산 결과에서 num[depth]의 값을 더하고 pluse의 개수를 하나 감소시킴
+    if minus:
+        dfs(depth + 1, total - num[depth], plus, minus - 1, multiply, divide)
+    if multiply:
+        dfs(depth + 1, total * num[depth], plus, minus, multiply - 1, divide)
+    if divide:
+        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide - 1)
+
+
+dfs(1, num[0], op[0], op[1], op[2], op[3])
+print(maximum)
+print(minimum)
